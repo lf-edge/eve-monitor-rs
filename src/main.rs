458d1435cb5b3a9d11857eb7bec74686a49fa4b5
Mikhail Malyshev <mike.malyshev@gmail.com>
2024-07-17 23:05:39 +0200

    Replace pretty_env_logger with log2
    We must write logs to a file only becasue we do not want TUI to be corrupted
    
    TODO: log2 is pretty dumb. I'd like a loger that can do colored output to a file
    and this can be configured
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,42 @@ mod terminal;
 mod traits;
 mod ui;
 
-use anyhow::{Ok, Result};
-use application::Application;
-use log::LevelFilter;
-use pretty_env_logger::env_logger::WriteStyle;
-use pretty_env_logger::formatted_builder;
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
-fn init_logging() {
-    formatted_builder()
-        .filter(None, LevelFilter::Trace)
-        .write_style(WriteStyle::Always)
-        .init();
+use anyhow::Result;
+use application::Application;
+use log::{info, LevelFilter};
+
+fn init_logging() -> log2::Handle {
+    let log_dir = if let Ok(_dir) = std::env::var("XDG_RUNTIME_DIR") {
+        // store log in the current directory for convenience
+        // we use XDG_RUNTIME_DIR to detect the fact that we are running on desktop linux
+        PathBuf::from("./")
+    } else {
+        PathBuf::from("/run")
+    };
+
+    let log_file = log_dir.join("./monitor.log").to_string_lossy().to_string();
+
+    let handle = log2::open(&log_file)
+        .size(10 * 1024 * 1024)
+        .rotate(20)
+        .tee(false) // no console output
+        .module(true)
+        .level(LevelFilter::Debug)
+        .start();
+
+    info!("Logging initialized: {:?}", log_file);
+
+    handle
 }
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
-    init_logging();
+    let _log2 = init_logging();
+
     let mut app = Application::new()?;
     app.run().await?;
     Ok(())

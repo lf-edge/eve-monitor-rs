2151fdfee4e796f75d9762835d7f1a65816f9039
Mikhail Malyshev <mike.malyshev@gmail.com>
2025-01-29 17:05:31 +0100

    Add build.rs to set GIT_VERSION cargo env variable
    - set GIT_VERSION env variable to display and log git version. It is not
      set if .git directory doesn't exist e.g. when building from snapshot
    or in container
    
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

---
+++ build.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::process::Command;
+
+fn main() {
+    // if .git doesnt exist then we are not in a git repo
+    // it may happen in container builds. do not set GIT_VERSION
+    if !std::path::Path::new(".git").exists() {
+        return;
+    }
+
+    // Get exact tag if it exists
+    let exact_tag = Command::new("git")
+        .args(["describe", "--tags", "--exact-match"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .map(|output| String::from_utf8_lossy(&output.stdout).trim().to_string());
+
+    let dirty_descr = Command::new("git")
+        .args(["describe", "--tags", "--dirty"])
+        .output()
+        .ok()
+        .filter(|output| output.status.success())
+        .map(|output| String::from_utf8_lossy(&output.stdout).trim().to_string())
+        .unwrap_or_else(|| "unknown".to_string());
+
+    let git_info = exact_tag.unwrap_or(dirty_descr);
+
+    // Set an environment variable in the Rust build output
+    println!("cargo:rustc-env=GIT_VERSION={}", git_info);
+}
